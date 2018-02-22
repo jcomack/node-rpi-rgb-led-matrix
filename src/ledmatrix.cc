5cd7a8cd5581be81f9aea3202178f0b8a49f762d
Jimmy Comack <jimmy@yoast.com>
2018-02-22 22:23:10 +0100

    Another attempt

--- src/ledmatrix.cc
+++ src/ledmatrix.cc
@@ -186,7 +186,7 @@ void LedMatrix::New(const Nan::FunctionCallbackInfo<Value>& args) {
    }
 
 		 if (Nan::HasOwnProperty(object, Nan::New<v8::String>("led_rgb_sequence").ToLocalChecked()).IsJust() ) {
-    options.led_rgb_sequence = *Nan::AsciiString(object->Get(Nan::New("led_rgb_sequence").ToLocalChecked()));
+    options.led_rgb_sequence = *Nan::CString(object->Get(Nan::New("led_rgb_sequence").ToLocalChecked()));
    }
 	}
 
